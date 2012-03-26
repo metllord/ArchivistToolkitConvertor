51ba51d64a07e2d6b090daabc71f777156c24916
unknown <estein@.DUCOM.edu>
2012-03-26 11:47:39 -0400

    added convert.py -- tested, but fails handling the multiple nameTypes

---
+++ convert.py
@@ -0,0 +1,83 @@
+import csv
+from sys import argv
+import os
+
+def xmloutput(source):
+    header = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<accessionRecords xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+"""
+    footer = "</accessionRecords>"
+    basicdata = """
+<record>
+<accessionNumber>
+<part1>%(part1)s</part1>
+<part2>%(part2)s</part2>
+<part3>%(part3)s</part3>
+</accessionNumber>
+<accessionDate>%(accessionDate)s</accessionDate>
+<acquisitionType>%(acquisitionType)s</acquisitionType>
+<userDefinedReal1>%(userDefinedReal1)s</userDefinedReal1>
+<extentNumber>%(extentNumber)s</extentNumber>
+<extentType>%(extentType)s</extentType>
+<description>%(description)s</description>
+<cataloged>%(cataloged)s</cataloged>
+"""
+    personName = """
+<nameLink>
+<nameLinkFunction>Source</nameLinkFunction>
+<name>
+<nameType>Person</nameType>
+<personalPrimaryName></personalPrimaryName>
+<personalRestOfName></personalRestOfName>
+<personalPrefix></personalPrefix>
+<personalSuffix></personalSuffix>
+</name>
+</nameLink>
+</record>"""
+    corportateName = """
+<nameLink>
+<nameLinkFunction>%(nameLinkFunction)s</nameLinkFunction>
+<name>
+<nameType>%(nameType)s</nameType>
+<corporatePrimaryName>%(corporatePrimaryName)s</corporatePrimaryName>
+<corporateSubordinate1>%(corporateSubordinate1)s</corporateSubordinate1>
+<corporateSubordinate2>%(corporateSubordinate2)s</corporateSubordinate2>
+</name>
+</nameLink>
+</record>"""
+
+    sourceReader = csv.DictReader(open(source, 'r'), delimiter="\t", quotechar='"')
+    output = open(os.path.splitext(source)[0] + '.xml', 'w')
+    stream = ''
+    stream += header
+    for row in sourceReader:
+        stream += basicdata % row
+        if row['nameType'] == 'Corporate Body':
+            stream += corportateName % row
+        elif row['nameType'] == 'Person':
+            stream += personName % row
+    stream += footer
+    output.write(stream)
+    output.close()
+
+
+
+
+
+if argv[1] == '*':
+    try:
+        os.mkdir('completed')
+    except WindowsError:
+        pass
+    filelist = [x for x in os.listdir('.') if os.path.splitext(x)[1] == '.tab']
+    for tabfile in filelist:
+        xmloutput(tabfile)
+        os.rename(tabfile, 'completed/'+tabfile)
+else:
+    try:
+        os.mkdir('completed')
+    except WindowsError:
+        pass
+    for tabfile in argv[1:]:
+        xmloutput(tabfile)
+        os.rename(tabfile, 'completed/'+tabfile)
--- readme.rst
+++ readme.rst
@@ -12,14 +12,14 @@ Usage
 1. Use Excel to ouput worksheets to tab delimited files. Save these files in a directory and place the
 convert.py in the same directory.
 
-2. Run the command python convert.py. To convert one document, specify that document as the first argument.
+2. Run the command python convert.py. To convert one or more documents, specify that document as the argument.
 To convert the entire directory, pass '*' as the first arguement.
 
 EX:
 
 .. sourcecode::
-    $ python convert.py SourceFile.tab
-    $ python convert.py *
+    $ python convert.py SourceFile.tab OtherSource.tab (converts these two files)
+    $ python convert.py * (Converts all files in working directory)
 
 3. The program will move the source files for the completed conversions to the Finished folder. The
 XML files will be placed in the working directory. If there are errors, they will be listed in errors.txt
